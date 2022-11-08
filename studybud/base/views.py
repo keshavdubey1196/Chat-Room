41f9a02e34a0567e8d8a322bbb72ce184ec5cfab
keshav <keshavsharma1196@gmail.com>
2022-11-09 01:56:28 +0530

    Chat room messages/ added all the features

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -128,3 +128,16 @@ def deleteRoom(request,pk):
         return redirect("home")
     return render(request, "base/delete.html",{"obj":room})
      
+
+#To delete message of a user in a room and make changes to the database:
+@login_required(login_url="login")
+def deleteMessage(request,pk):
+    message=Message.objects.get(id=pk)
+    
+    if request.user!= message.user:
+        return HttpResponse("You are not allowed here!!")
+
+    if request.method == "POST":
+        message.delete()
+        return redirect("home")
+    return render(request, "base/delete.html",{"obj":message})
