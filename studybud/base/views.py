4c06e7e29305e26c8f259794b359bc075c8828ae
keshav <keshavsharma1196@gmail.com>
2022-11-09 01:42:21 +0530

    chat room messages/adding participants to homepage

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 # Create your views here.
@@ -70,8 +70,19 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages=room.message_set.all()
-    context = {"room": room,"room_messages": room_messages}
+    room_messages=room.message_set.all().order_by("-created")
+    participants=room.participants.all()
+
+    if request.method == "POST":
+        message=Message.objects.create(
+            user=request.user, 
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect("room",pk=room.id)
+
+    context = {"room": room,"room_messages": room_messages,"participants": participants}
     return render(request, "base/room.html", context)
 
 
