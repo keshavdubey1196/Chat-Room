610f790d168159d9e23851ab7d3e07af4a994218
keshav <keshavsharma1196@gmail.com>
2022-11-13 16:43:45 +0530

    MobileResponsiveness/ complete with some errors, fix at the end

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -62,11 +62,9 @@ def home(request):
                                 Q(name__icontains=q) | 
                                 Q(description__icontains=q) )
 
-    topics=Topic.objects.all()
+    topics=Topic.objects.all()[0:5]
     room_count=rooms.count()  
-    room_messages=Message.objects.filter(Q(
-                                           room__topic__name__icontains=q                  
-    )) 
+    room_messages=Message.objects.filter(Q(room__topic__name__icontains=q)) 
     context = {"rooms": rooms,"topics": topics,"room_count": room_count,
                 "room_messages": room_messages}
     return render(request, "base/home.html", context)
@@ -185,3 +183,14 @@ def updateUser(request):
 
     context={"form": form}
     return render(request, "base/update-user.html",context)
+
+def topicsPage(request):
+    q=request.GET.get('q') if request.GET.get('q') != None else "" 
+    topics=Topic.objects.filter(name__icontains=q) # filter() without args will act as all()
+    context={"topics": topics}
+    return render(request, "base/topics.html",context)
+
+def activityPage(request):
+    room_messages=Message.objects.all()
+    context={"room_messages": room_messages}
+    return render(request, "base/activity.html",context)
